c9d4c52dfaf39656e47fd472a69b037de4e6b454
scott <rsmckinney@hotmail.com>
2022-02-11 22:22:31 -0500

    update manifold version

---
+++ src/main/resources/public/js/app.js
@@ -0,0 +1,11 @@
+$(document)
+    .keyup('input', function (e) {
+        if (e.keyCode === 27) {
+            $(e.target).trigger('resetEscape');
+        }
+    })
+    .click(function (e) {
+        if ($('#todo-edit').length > 0 && !$(e.target).is('#todo-edit')) {
+            $("#edit-form").trigger('submit');
+        }
+    });
